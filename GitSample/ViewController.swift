bd9347f2708078e7d3bdc498334fdb8127f97b2e
kamui_project <starmine2013@gmail.com>
2018-06-17 18:27:46 +0900

    Add Project File

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Morita Jun on 2018/06/17.
+//  Copyright © 2018 KamuiProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
